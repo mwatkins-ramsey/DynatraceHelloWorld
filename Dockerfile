2c3c8837fc1300f6dd6a7c1aeba9dad26452a3fc
Michael Watkins <130600163+mwatkins-ramsey@users.noreply.github.com>
2023-07-19 16:26:06 -0500

    Initial Upload

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:7.0 as builder
+
+COPY . /src
+WORKDIR "/src"  
+RUN  dotnet restore "DynatraceHelloWorld.sln" &&  \
+     dotnet build "DynatraceHelloWorld.sln" &&  \
+     dotnet test "DynatraceHelloWorld.sln"  &&  \
+     dotnet publish "DynatraceHelloWorld/DynatraceHelloWorld.csproj" -c Release -o /src/out
+  
+# Build runtime image  
+FROM mcr.microsoft.com/dotnet/aspnet:7.0 as prod
+ENV API_PORT=5000
+WORKDIR /src  
+COPY --from=builder /src/out .  
+ENTRYPOINT ["dotnet", "DynatraceHelloWorld.dll"]
---
+++ DynatraceHelloWorld/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace DynatraceHelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var port = GetPortFromEnvironment() ?? 5000;
+            var host = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.ListenAnyIP(port);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    // Add routing services
+                    services.AddRouting();
+                })
+                .Configure(app =>
+                {
+                    app.UseRouting();
+
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapGet("/hello", async context =>
+                        {
+                            context.Response.ContentType = "application/json";
+                            await context.Response.WriteAsync("{\"message\":\"Hello World\"}");
+                        });
+                    });
+                })
+                .Build();
+
+            host.Run();
+        }
+        
+        private static int? GetPortFromEnvironment()
+        {
+            var portString = Environment.GetEnvironmentVariable("API_PORT");
+            if (int.TryParse(portString, out int port))
+            {
+                return port;
+            }
+            Console.WriteLine("API_PORT not set in env, resorting to default");
+            return null;
+        }
+    }
+}
