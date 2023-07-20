b40c9979e1587bbfe3d976dbadc14256b5836b45
Michael Watkins <130600163+mwatkins-ramsey@users.noreply.github.com>
2023-07-20 13:29:43 -0500

    using ramsey docker base and oneagentSDK, still not working

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
-﻿FROM mcr.microsoft.com/dotnet/sdk:7.0 as builder
+﻿FROM 058238361356.dkr.ecr.us-east-1.amazonaws.com/innersource/dotnet/rs-dotnet70:IR.1.0.0-builder-2023-06-30-T19.36.13 as build
 
 COPY . /src
 WORKDIR "/src"  
 RUN  dotnet restore "DynatraceHelloWorld.sln" &&  \
-     dotnet build "DynatraceHelloWorld.sln" &&  \
-     dotnet test "DynatraceHelloWorld.sln"  &&  \
+     dotnet build "DynatraceHelloWorld.sln" -c Release --no-restore &&  \
+     dotnet test "DynatraceHelloWorld.sln" -c Release --no-build &&  \
      dotnet publish "DynatraceHelloWorld/DynatraceHelloWorld.csproj" -c Release -o /src/out
   
 # Build runtime image  
-FROM mcr.microsoft.com/dotnet/aspnet:7.0 as prod
+FROM 058238361356.dkr.ecr.us-east-1.amazonaws.com/innersource/dotnet/rs-dotnet70:IR.1.0.0-2023-06-30-T19.36.27 as prod
 ENV API_PORT=5000
 WORKDIR /src  
-COPY --from=builder /src/out .  
+COPY --from=build /src/out .  
 ENTRYPOINT ["dotnet", "DynatraceHelloWorld.dll"]
--- DynatraceHelloWorld/Program.cs
+++ DynatraceHelloWorld/Program.cs
@@ -4,19 +4,74 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using Dynatrace.OneAgent.Sdk.Api;
+using Dynatrace.OneAgent.Sdk.Api.Enums;
+using Dynatrace.OneAgent.Sdk.Api.Infos;
+using Microsoft.Extensions.Logging;
 
 namespace DynatraceHelloWorld
 {
     class Program
     {
+        private static string
+            DT_API_URL =
+                Environment.GetEnvironmentVariable("DYINTRACE_API_URL") ?? "";
+
+        private static string
+            DT_API_TOKEN =
+                Environment.GetEnvironmentVariable("DYINTRACE_API_TOKEN") ?? "";
+
+        private const string
+            activitySource = "MWatkins.DyinTrace.Lab";
+
+        public static readonly ActivitySource MyActivitySource = new ActivitySource(activitySource);
+
         public static void Main(string[] args)
         {
+            IOneAgentSdk oneAgentSdk = OneAgentSdkFactory.CreateInstance();
+            var loggingCallback = new StdErrLoggingCallback();
+            oneAgentSdk.SetLoggingCallback(loggingCallback);
+            IOneAgentInfo agentInfo = oneAgentSdk.AgentInfo;
+            initOpenTelemetry();
+            IOneAgentInfo agentInfo2 = oneAgentSdk.AgentInfo;
+            if (agentInfo2.AgentFound)
+            {
+                Console.WriteLine($"OneAgent Version: {agentInfo2.Version}");
+                if (agentInfo2.AgentCompatible)
+                {
+                    Console.WriteLine("ITS ALIVE!!");
+                }
+            }
+            SdkState state = oneAgentSdk.CurrentState;
+            switch (state)
+            {
+                case SdkState.ACTIVE:
+                    Console.WriteLine("sdk active");
+                    break;
+                case SdkState.TEMPORARILY_INACTIVE: // capturing disabled, tracing calls can be spared
+                    Console.WriteLine("sdk inactive");
+                    break;
+                case SdkState.PERMANENTLY_INACTIVE: // SDK permanently inactive, tracing calls can be spared
+                    Console.WriteLine("sdk permanently inactive");
+                    break;
+            }
+            
+            
+            
+            Meter meter = new Meter("hello-count", "1.0.0");
+            Counter<long> counter = meter.CreateCounter<long>("request_counter");
+
             var port = GetPortFromEnvironment() ?? 5000;
             var host = new WebHostBuilder()
-                .UseKestrel(options =>
-                {
-                    options.ListenAnyIP(port);
-                })
+                .UseKestrel(options => { options.ListenAnyIP(port); })
                 .ConfigureServices((context, services) =>
                 {
                     // Add routing services
@@ -30,8 +85,26 @@ namespace DynatraceHelloWorld
                     {
                         endpoints.MapGet("/hello", async context =>
                         {
+                            using var activity = MyActivitySource.StartActivity("Call to /hello");
+                            activity?.SetTag("http.method", "GET");
+                            activity?.SetTag("net.protocol.version", "1.1");
+                            counter.Add(1, new System.Collections.Generic.KeyValuePair<string,object?>("hello", "hello was called!"));
                             context.Response.ContentType = "application/json";
+                            
+                            ITraceContextInfo traceContextInfo = oneAgentSdk.TraceContextInfo;
+                            string traceId = traceContextInfo.TraceId;
+                            string spanId = traceContextInfo.SpanId;
+                            Console.WriteLine("TID:" + traceId +", SID:" + spanId + " valid?:" + traceContextInfo.IsValid);
+                            
                             await context.Response.WriteAsync("{\"message\":\"Hello World\"}");
+                        });
+
+                        endpoints.MapGet("/ready", async context =>
+                        {
+                            using var activity = MyActivitySource.StartActivity("Call to /ready");
+                            activity?.SetTag("http.method", "GET");
+                            activity?.SetTag("net.protocol.version", "1.1");
+                            await context.Response.WriteAsync("");
                         });
                     });
                 })
@@ -39,7 +112,7 @@ namespace DynatraceHelloWorld
 
             host.Run();
         }
-        
+
         private static int? GetPortFromEnvironment()
         {
             var portString = Environment.GetEnvironmentVariable("API_PORT");
@@ -47,8 +120,85 @@ namespace DynatraceHelloWorld
             {
                 return port;
             }
+
             Console.WriteLine("API_PORT not set in env, resorting to default");
             return null;
         }
+
+        private static void initOpenTelemetry()
+        {
+            // ===== GENERAL SETUP =====
+
+            List<KeyValuePair<string, object>> dt_metadata = new List<KeyValuePair<string, object>>();
+            foreach (string name in new string[]
+                     {
+                         "dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties",
+                         "/var/lib/dynatrace/enrichment/dt_metadata.properties"
+                     })
+            {
+                try
+                {
+                    foreach (string line in System.IO.File.ReadAllLines(name.StartsWith("/var")
+                                 ? name
+                                 : System.IO.File.ReadAllText(name)))
+                    {
+                        var keyvalue = line.Split("=");
+                        dt_metadata.Add(new KeyValuePair<string, object>(keyvalue[0], keyvalue[1]));
+                    }
+                }
+                catch
+                {
+                }
+            }
+
+            Action<ResourceBuilder> configureResource = r => r
+                .AddService(serviceName: "Mwatkins-Dyintrace-lab")
+                .AddAttributes(dt_metadata);
+
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+
+            // ===== TRACING SETUP =====
+
+            var tracerBuilder = Sdk.CreateTracerProviderBuilder()
+                .ConfigureResource(configureResource)
+                .SetSampler(new AlwaysOnSampler())
+                .AddOtlpExporter(otlpOptions =>
+                {
+                    otlpOptions.Endpoint = new Uri(DT_API_URL + "/v1/traces");
+                    otlpOptions.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+                    otlpOptions.Headers = $"Authorization=Api-Token {DT_API_TOKEN}";
+                    otlpOptions.ExportProcessorType = ExportProcessorType.Batch;
+                })
+                .AddSource(MyActivitySource.Name);
+
+            tracerBuilder.Build();
+
+
+            // ===== METRIC SETUP =====
+
+            Sdk.CreateMeterProviderBuilder()
+                .ConfigureResource(configureResource)
+                .AddMeter("my_meter")
+                .AddOtlpExporter((OtlpExporterOptions exporterOptions, MetricReaderOptions readerOptions) =>
+                {
+                    exporterOptions.Endpoint = new Uri(DT_API_URL + "/v1/metrics");
+                    exporterOptions.Headers = $"Authorization=Api-Token {DT_API_TOKEN}";
+                    exporterOptions.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+                    readerOptions.TemporalityPreference = MetricReaderTemporalityPreference.Delta;
+                })
+                .Build();
+
+
+            // ===== LOG SETUP =====
+
+            var resourceBuilder = ResourceBuilder.CreateDefault();
+            configureResource!(resourceBuilder);
+        }
+    }
+    class StdErrLoggingCallback : ILoggingCallback
+    {
+        public void Error(string message) => Console.Error.WriteLine("[OneAgent SDK] Error:   " + message);
+        public void Warn (string message) => Console.Error.WriteLine("[OneAgent SDK] Warning: " + message);
     }
 }
